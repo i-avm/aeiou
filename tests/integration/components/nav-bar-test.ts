36ddadc6f868d2cdffb22adde66907633b360089
Abin Varghese <abin.varghese@mitsogo.com>
2022-06-22 19:45:03 +0530

    package(tailwindcss): add tailwind css package

---
+++ tests/integration/components/nav-bar-test.ts
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | nav-bar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`{{nav-bar}}`);
+
+    //TODO
+  });
+});
